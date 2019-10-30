fe856293a2469cd9712602fa92f2991ae9c50ea4
Clement Mahlangu <clement.mahlangu@gmail.com>
2019-10-30 22:46:11 +0200

    Work in Progress

--- source/conf.py
+++ source/conf.py
@@ -3,91 +3,32 @@ import sys
 import os
 import sphinx_bootstrap_theme
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
-
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.todo',
-	'rst2pdf.pdfbuilder',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# The suffix of source filenames.
 source_suffix = '.rst'
-
-
-# The master toctree document.
 master_doc = 'index'
-
-# General information about the project.
 project = u'DraZNotes'
-copyright = u'2017, Clement Thulani Mahlangu'
+copyright = u'2019, Clement Thulani Mahlangu'
 
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
 version = '0.1'
-# The full version, including alpha/beta/rc tags.
 release = '0.1'
 
-# There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-#today = ''
-# Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
-
-# List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build', 'README.rst']
-
-# The reST default role (used for this markup: `text`) to use for all
-# documents.
-#default_role = None
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-
-# If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
-#add_module_names = True
-
-# If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-#show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
-
-# If true, keep warnings as "system message" paragraphs in the built documents.
-#keep_warnings = False
-
 
 # -- Options for HTML output ----------------------------------------------
 
-# Build in themes -> 'default', 'sphinxdoc', 'scrolls','agogo','traditional'
-#  'haiku', 'pyramid'
 html_theme = 'bootstrap'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
